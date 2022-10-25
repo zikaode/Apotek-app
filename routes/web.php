67f4c647fa2dca297eae9caab0f2851c64fe2909
Dzikri Arraiyan <dzikriarraiyan@gmail.com>
2022-10-25 20:30:50 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
